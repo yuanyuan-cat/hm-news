dc8a732b8bce58f540b4ad5b0bfee2519e46c4ce
yuanyuan-cat <704824771@qq.com>
2020-08-29 16:19:29 +0800

    user头部静态页面完成

---
+++ src/views/User.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="user">
+    <!-- 头部 -->
+    <div class="header">
+      <div class="avatar">
+        <img src="../assets/1.gif" alt="">
+      </div>
+      <div class="info">
+        <span class="iconfont iconxingbienan"></span>
+        <span>火星网友</span>
+        <div class="time">2019-10-10</div>
+      </div>
+      <div class="arrow">
+        <span class="iconfont iconjiantou1"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+  .user {
+    border-top: 40px solid #666;
+    .header {
+      display: flex;
+      align-items: center;
+      padding: 20px 10px;
+      font-size: 16px;
+      border-bottom: 5px solid #ccc;
+      .avatar img {
+        width: 70px;
+        height: 70px;
+        border-radius: 50%;
+      }
+      .info {
+        flex: 1;
+        margin-left: 20px;
+        .iconxingbienan {
+          margin-right: 5px;
+          color: skyblue;
+        }
+        .time {
+          margin-top: 5px;
+          color: #ccc;
+        }
+      }
+    }
+  }
+</style>
