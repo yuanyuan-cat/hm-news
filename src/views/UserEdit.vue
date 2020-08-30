2765224bbe52376ff8c5423d1af581d93049a378
yuanyuan-cat <704824771@qq.com>
2020-08-30 17:56:27 +0800

    渲染后台数据完成

--- src/views/UserEdit.vue
+++ src/views/UserEdit.vue
@@ -4,11 +4,11 @@
     <hm-header>编辑资料</hm-header>
     <!-- 头像 -->
     <div class="avatar">
-      <img src="../assets/1.gif" alt="">
+      <img :src="$axios.defaults.baseURL + user.head_img" alt="">
     </div>
     <hm-navitem>
       <template>昵称</template>
-      <template #content>火星网友</template>
+      <template #content>{{user.nickname}}</template>
     </hm-navitem>
     <hm-navitem>
       <template>密码</template>
@@ -16,14 +16,33 @@
     </hm-navitem>
     <hm-navitem>
       <template>性别</template>
-      <template #content>男</template>
+      <template #content>{{user.gender === 0 ? '女' : '男'}}</template>
     </hm-navitem>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      user: ''
+    }
+  },
+  created () {
+    this.updateUser()
+  },
+  methods: {
+    // 封装一个发送ajax请求获取渲染数据的函数
+    async updateUser () {
+      const id = localStorage.getItem('userId')
+      const res = await this.$axios.post(`/user_update/${id}`)
+      const { data, statusCode } = res.data
+      // 请求成功后，获取渲染的数据
+      if (statusCode === 200) {
+        this.user = data
+      }
+    }
+  }
 }
 </script>
 
